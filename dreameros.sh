83f91d15464a9c5c50aa21b338d61f6e9783060f
Deven-Eve <63565512+Deven-Eve@users.noreply.github.com>
2023-04-19 02:48:31 +0200

    Create dreameros.sh

---
+++ dreameros.sh
@@ -0,0 +1,287 @@
+timedatectl set-ntp true
+Setting up mirrors for optimal download
+"
+source $CONFIGS_DIR/setup.conf
+iso=$(curl -4 ifconfig.co/country-iso)
+timedatectl set-ntp true
+pacman -S --noconfirm archlinux-keyring #update keyrings to latest to prevent packages failing to install
+pacman -S --noconfirm --needed pacman-contrib terminus-font
+setfont ter-v22b
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+pacman -S --noconfirm --needed reflector rsync grub
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+echo -ne "
+-------------------------------------------------------------------------
+                    Setting up $iso mirrors for faster downloads
+-------------------------------------------------------------------------
+"
+reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+mkdir /mnt &>/dev/null # Hiding error message if any
+echo -ne "
+-------------------------------------------------------------------------
+                    Installing Prerequisites
+-------------------------------------------------------------------------
+"
+pacman -S --noconfirm --needed gptfdisk btrfs-progs glibc
+echo -ne "
+-------------------------------------------------------------------------
+                    Formating Disk
+-------------------------------------------------------------------------
+"
+umount -A --recursive /mnt # make sure everything is unmounted before we start
+# disk prep
+sgdisk -Z ${DISK} # zap all on disk
+sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+
+# create partitions
+sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
+sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
+sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
+if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
+    sgdisk -A 1:set:2 ${DISK}
+fi
+partprobe ${DISK} # reread partition table to ensure it is correct
+
+# make filesystems
+echo -ne "
+-------------------------------------------------------------------------
+                    Creating Filesystems
+-------------------------------------------------------------------------
+"
+# @description Creates the btrfs subvolumes. 
+createsubvolumes () {
+    btrfs subvolume create /mnt/@
+    btrfs subvolume create /mnt/@home
+    btrfs subvolume create /mnt/@var
+    btrfs subvolume create /mnt/@tmp
+    btrfs subvolume create /mnt/@.snapshots
+}
+
+# @description Mount all btrfs subvolumes after root has been mounted.
+mountallsubvol () {
+    mount -o ${MOUNT_OPTIONS},subvol=@home ${partition3} /mnt/home
+    mount -o ${MOUNT_OPTIONS},subvol=@tmp ${partition3} /mnt/tmp
+    mount -o ${MOUNT_OPTIONS},subvol=@var ${partition3} /mnt/var
+    mount -o ${MOUNT_OPTIONS},subvol=@.snapshots ${partition3} /mnt/.snapshots
+}
+
+# @description BTRFS subvolulme creation and mounting. 
+subvolumesetup () {
+# create nonroot subvolumes
+    createsubvolumes     
+# unmount root to remount with subvolume 
+    umount /mnt
+# mount @ subvolume
+    mount -o ${MOUNT_OPTIONS},subvol=@ ${partition3} /mnt
+# make directories home, .snapshots, var, tmp
+    mkdir -p /mnt/{home,var,tmp,.snapshots}
+# mount subvolumes
+    mountallsubvol
+}
+
+if [[ "${DISK}" =~ "nvme" ]]; then
+    partition2=${DISK}p2
+    partition3=${DISK}p3
+else
+    partition2=${DISK}2
+    partition3=${DISK}3
+fi
+
+if [[ "${FS}" == "btrfs" ]]; then
+    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
+    mkfs.btrfs -L ROOT ${partition3} -f
+    mount -t btrfs ${partition3} /mnt
+    subvolumesetup
+elif [[ "${FS}" == "ext4" ]]; then
+    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
+    mkfs.ext4 -L ROOT ${partition3}
+    mount -t ext4 ${partition3} /mnt
+elif [[ "${FS}" == "luks" ]]; then
+    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
+# enter luks password to cryptsetup and format root partition
+    echo -n "${LUKS_PASSWORD}" | cryptsetup -y -v luksFormat ${partition3} -
+# open luks container and ROOT will be place holder 
+    echo -n "${LUKS_PASSWORD}" | cryptsetup open ${partition3} ROOT -
+# now format that container
+    mkfs.btrfs -L ROOT ${partition3}
+# create subvolumes for btrfs
+    mount -t btrfs ${partition3} /mnt
+    subvolumesetup
+# store uuid of encrypted partition for grub
+    echo ENCRYPTED_PARTITION_UUID=$(blkid -s UUID -o value ${partition3}) >> $CONFIGS_DIR/setup.conf
+fi
+
+# mount target
+mkdir -p /mnt/boot/efi
+mount -t vfat -L EFIBOOT /mnt/boot/
+
+if ! grep -qs '/mnt' /proc/mounts; then
+    echo "Drive is not mounted can not continue"
+    echo "Rebooting in 3 Seconds ..." && sleep 1
+    echo "Rebooting in 2 Seconds ..." && sleep 1
+    echo "Rebooting in 1 Second ..." && sleep 1
+    reboot now
+fi
+echo -ne "
+-------------------------------------------------------------------------
+                    Arch Install on Main Drive
+-------------------------------------------------------------------------
+"
+pacstrap /mnt base base-devel linux linux-firmware nano sudo archlinux-keyring wget libnewt --noconfirm --needed
+echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
+cp -R ${SCRIPT_DIR} /mnt/root/ArchTitus
+cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+
+genfstab -L /mnt >> /mnt/etc/fstab
+echo " 
+  Generated /etc/fstab:
+"
+cat /mnt/etc/fstab
+echo -ne "
+-------------------------------------------------------------------------
+                    GRUB BIOS Bootloader Install & Check
+-------------------------------------------------------------------------
+"
+if [[ ! -d "/sys/firmware/efi" ]]; then
+    grub-install --boot-directory=/mnt/boot ${DISK}
+else
+    pacstrap /mnt efibootmgr --noconfirm --needed
+fi
+echo -ne "
+-------------------------------------------------------------------------
+                    Checking for low memory systems <8G
+-------------------------------------------------------------------------
+"
+TOTAL_MEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
+if [[  $TOTAL_MEM -lt 8000000 ]]; then
+    # Put swap into the actual system, not into RAM disk, otherwise there is no point in it, it'll cache RAM into RAM. So, /mnt/ everything.
+    mkdir -p /mnt/opt/swap # make a dir that we can apply NOCOW to to make it btrfs-friendly.
+    chattr +C /mnt/opt/swap # apply NOCOW, btrfs needs that.
+    dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=2048 status=progress
+    chmod 600 /mnt/opt/swap/swapfile # set permissions.
+    chown root /mnt/opt/swap/swapfile
+    mkswap /mnt/opt/swap/swapfile
+    swapon /mnt/opt/swap/swapfile
+    # The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the system itself.
+    echo "/opt/swap/swapfile	none	swap	sw	0	0" >> /mnt/etc/fstab # Add swap to fstab, so it KEEPS working after installation.
+fi
+echo -ne "
+-------------------------------------------------------------------------
+                    SYSTEM READY FOR 1-setup.sh
+-------------------------------------------------------------------------
+"
+-------------------------------------------------------------------------
+                    Network Setup 
+-------------------------------------------------------------------------
+"
+pacman -S --noconfirm --needed networkmanager dhclient
+systemctl enable --now NetworkManager
+echo -ne "
+-------------------------------------------------------------------------
+                    Setting up mirrors for optimal download 
+-------------------------------------------------------------------------
+"
+pacman -S --noconfirm --needed pacman-contrib curl
+pacman -S --noconfirm --needed reflector rsync grub arch-install-scripts git
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+
+nc=$(grep -c ^processor /proc/cpuinfo)
+echo -ne "
+-------------------------------------------------------------------------
+                    You have " $nc" cores. And
+			changing the makeflags for "$nc" cores. Aswell as
+				changing the compression settings.
+-------------------------------------------------------------------------
+"
+TOTAL_MEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
+if [[  $TOTAL_MEM -gt 8000000 ]]; then
+sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$nc\"/g" /etc/makepkg.conf
+sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g" /etc/makepkg.conf
+fi
+echo -ne "
+-------------------------------------------------------------------------
+                    Setup Language to US and set locale  
+-------------------------------------------------------------------------
+"
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+timedatectl --no-ask-password set-timezone ${TIMEZONE}
+timedatectl --no-ask-password set-ntp 1
+localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
+ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+# Set keymaps
+localectl --no-ask-password set-keymap ${KEYMAP}
+
+# Add sudo no password rights
+sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+
+#Add parallel downloading
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+
+#Enable multilib
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+pacman -Sy --noconfirm --needed
+echo -ne "
+-------------------------------------------------------------------------
+                    Installing Base System  
+-------------------------------------------------------------------------
+"
+pacman -S icewm xorg-server xorg-xinit libxinerama libxft focuswriter udiskie light plymouth
+sudo rm -R /usr/share/icewm/themes/NanoBlue /usr/share/icewm/themes/CrystalBlue /usr/share/icewm/themes/icedesert /usr/share/icewm/themes/metal2 /usr/share/icewm/themes/motif /usr/share/icewm/themes/win95 /usr/share/icewm/themes/default /usr/share/icewm/themes/Infadel2
+sudo mv Helix black
+sudo cat << EOF > /usr/share/icewm/theme/black/default.theme
+ThemeDescription="redice"
+ThemeAuthor="RudeSka... Sort of.  TigerT did the art"
+Look=gtk
+
+TitleButtonsLeft="s"
+TitleButtonsRight="xmi"
+TitleBarCentered=1
+TitleBarHeight=22
+BorderSizeX=2
+BorderSizeY=2
+CornerSizeX=28
+CornerSizeY=28
+DlgBorderSizeX=2
+DlgBorderSizeY=2
+
+ColorNormalBorder="#000000"
+ColorActiveBorder="#000000"
+
+ColorNormalButton="#000000"
+ColorNormalButtonText="#FFFFFF"
+ColorActiveButton="#000000"
+ColorActiveButtonText="#FFFFFF"
+ColorNormalTitleBarText="#FFFFFF"
+ColorActiveTitleBarText="#FFFFFF"
+
+ColorNormalMenu="#000000"
+ColorActiveMenuItem="#000000"
+ColorNormalMenuItemText="#FFFFFF"
+ColorActiveMenuItemText="#FFFFFF"
+ColorDisabledMenuItemText="#FFFFFF"
+
+ColorMoveSizeStatus="#000000"
+ColorMoveSizeStatusText="#FFFFFF"
+
+ActiveButtonFontName="-b&h-lucida-medium-r-*-*-12-*-*-*-*-*-*-*"
+NormalButtonFontName="-b&h-lucida-medium-r-*-*-12-*-*-*-*-*-*-*"
+ClockFontName="-b&h-lucida-medium-r-*-*-12-*-*-*-*-*-*-*"
+QuickSwitchFontName="-b&h-lucida-medium-r-*-*-11-*-*-*-*-*-*-*"
+ListBoxFontName="-b&h-lucida-medium-r-*-*-12-*-*-*-*-*-*-*"
+MenuFontName="-b&h-lucida-medium-r-*-*-12-*-*-*-*-*-*-*"
+StatusFontName="-b&h-lucida-medium-r-*-*-12-*-*-*-*-*-*-*"
+TitleFontName="-b&h-lucida-medium-r-*-*-12-*-*-*-*-*-*-*"
+ToolTipFontName="-b&h-lucida-medium-r-*-*-12-*-*-*-*-*-*-*"
+
+ShowMenuButtonIcon=0
+EOF
+
+
+
+
+
+
+
+
